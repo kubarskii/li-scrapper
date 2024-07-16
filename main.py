8b84c79a9e80e2e398d118cdc4be8adc7aa6d67e
Aleksandr Kubarskii <sasha.kub95@gmail.com>
2024-07-17 00:05:42 +0400

    feat, mouse moves

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from linkedin_scraper import Person, actions
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
 
 from urllib.parse import urlparse
 
@@ -10,6 +11,10 @@ import json
 
 import time
 import random
+
+
+def random_delay(min_delay=3, max_delay=8):
+    time.sleep(random.uniform(min_delay, max_delay))
 
 
 def login(driver, email, password):
@@ -29,18 +34,38 @@ def is_logged_in(driver):
         return False
 
 
-def random_delay(min_delay=1, max_delay=2):
-    time.sleep(random.uniform(min_delay, max_delay))
+def move_mouse_naturally(driver, element):
+    actions = ActionChains(driver)
+    element_location = element.location
+    element_size = element.size
+    start_x = random.randint(
+        0, driver.execute_script("return window.innerWidth"))
+    start_y = random.randint(
+        0, driver.execute_script("return window.innerHeight"))
+    end_x = element_location['x'] + element_size['width'] / 2
+    end_y = element_location['y'] + element_size['height'] / 2
+
+    steps = 100  # Number of steps to move the mouse
+    for i in range(steps):
+        x = start_x + (end_x - start_x) * i / steps
+        y = start_y + (end_y - start_y) * i / steps
+        actions.move_by_offset(x - actions._driver.w3c_actions.pointer_parameters.origin.x,
+                               y - actions._driver.w3c_actions.pointer_parameters.origin.y)
+        actions.perform()
+        random_delay(0.01, 0.05)
 
 
 driver = webdriver.Chrome()
 
-email = "@gmail.com"
+email = "gmail.com"
 password = ""
-# if email and password isnt given, it'll prompt in terminal
-actions.login(driver, email, password)
+
+# Perform initial login
+login(driver, email, password)
+
 driver.get('https://www.linkedin.com/search/results/people/?keywords=venture%20capital%20partners&network=%5B%22S%22%5D&origin=GLOBAL_SEARCH_HEADER&sid=jk%2C')
 links = []
+
 # Scroll and paginate through search results
 while True:
     driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
@@ -69,6 +94,9 @@ while True:
             By.CSS_SELECTOR, '.artdeco-pagination__button--next')
         if next_button.get_attribute('disabled'):
             break
+
+        move_mouse_naturally(driver, next_button)
+        random_delay()
         next_button.click()
         random_delay()
     except Exception as e:
@@ -83,8 +111,6 @@ people = []
 
 for url in links:
     if url is not None:
-        if not is_logged_in(driver):
-            login(driver, email, password)
         driver.get(url)
         random_delay()
         WebDriverWait(driver, 10).until(
